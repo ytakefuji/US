acc80856a6398c284fc2c645f3924ae47323a722
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2021-06-11 20:21:22 +0900

    Add files via upload

--- deathdaily.py
+++ deathdaily.py
@@ -1,28 +1,31 @@
 import pandas as pd
 import numpy as np
 import sys
+from time import sleep
 import matplotlib.pyplot as plt
 data=pd.read_csv("new_deaths.csv")
-def main(country="Japan",days=440, degree=5):
- n=len(data[country])
- y=data[country][n-days:n]
- for i in y:
-  print(i)
- x=np.arange(n-days,n)
- valid = ~(np.isnan(x) | np.isnan(y))
- model=np.poly1d(np.polyfit(x[valid],y[valid],degree))
- date=data['date'][n-1]
- x1=np.arange(n-days,n+7)
- y1=model(x1)
- ny1=[]
- for i in y1:
-  if i<0:i=0
-  ny1.append(i)
- plt.plot(x,y,'k')
- plt.plot(x1,ny1,'b')
- plt.legend(['daily deaths in '+str(country),str(days)+' days from '+str(date)+' '+str(degree)+'th'])
- plt.savefig(country+".png")
- plt.show()
+
+class main:
+ def main(self,country,days=440,degree=5):
+   n=len(data[country])
+   y=data[country][n-days:n]
+   for i in y:
+    print(i)
+   x=np.arange(n-days,n)
+   valid = ~(np.isnan(x) | np.isnan(y))
+   model=np.poly1d(np.polyfit(x[valid],y[valid],degree))
+   date=data['date'][n-1]
+   x1=np.arange(n-days,n+7)
+   y1=model(x1)
+   ny1=[]
+   for i in y1:
+    if i<0:i=0
+    ny1.append(i)
+   plt.plot(x,y,'k')
+   plt.plot(x1,ny1,'b')
+   plt.legend(['daily deaths in '+str(country),str(days)+' days from '+str(date)+' '+str(degree)+'th'])
+   plt.savefig(country+".png")
+   plt.show()
 country=""
 days=440
 degree=5
@@ -62,6 +65,6 @@ if len(sys.argv)==4:
  else:
   print('correct country name')
   sys.exit()
-  
-main(country,days,degree)
+m=main()  
+m.main(country=country,days=days,degree=degree)
  
